e203e8ac842629df938df799529680efeb05d69a
Alina <a.y.kositsyna@gmail.com>
2023-04-28 13:37:56 +0700

    created classes

---
+++ assignment2.4/ConsoleReporting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2._4
+{
+    public class ConsoleReporting
+    {
+        public void Parse(List<Object> objects) 
+        {
+            foreach (Object obj in objects)
+            {
+
+            }
+        }
+    }
+}
